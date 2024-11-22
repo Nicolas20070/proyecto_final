82b19d8cf9a2aca8595b28cf99af4afb5988bd3e
sofia-Caardenas <159914743+sofia-Caardenas@users.noreply.github.com>
2024-11-22 10:38:02 -0500

    token al registrar

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,58 +3,62 @@ import User from "../entities/user.entity.js"
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 
-export const userRegister = async (req, res)=>{
-    //desestructurar el body 
-    const {firstName,
-        lastName,
-        email,
-        password,
-        isAdmin} = req.body
+export const userRegister = async (req, res) => {
+    const { firstName, lastName, email, password, isAdmin } = req.body;
+    
+    const vUser = await User.findOne({ email: req.body.email });
+    if (vUser) {
+        return res.status(400).json({ message: "Ya existe el usuario" });
+    } else {
+        const sal = await bcrypt.genSalt(10);
+        const bcPassword = await bcrypt.hash(password, sal);
+        
+        const newUser = await User.create({
+            firstName,  
+            lastName,
+            email,
+            password: bcPassword,
+            isAdmin
+        });
+        
+        const token = generarToken(newUser._id);
 
-    //verificar si el usuario existe 
-    const vUser = await User.findOne({email: req.body.email})
-    if (vUser){
-        res.status(400).json({message: "ya existe el usaurio"})
-    }else{
-    //encriptar el password del body
-    const sal = await bcrypt.genSalt(10)
-    const bcPassword = await bcrypt.hash(password, sal)
-    //crear el nuevo usuario:
-    const newUser = await User.create({
-        firstName,  
-        lastName,
-        email,
-        password:bcPassword,
-        isAdmin
-    })
-        res.status(201).json(newUser)
+        res.status(201).json({
+            id: newUser._id,
+            firstName: newUser.firstName,
+            lastName: newUser.lastName,
+            email: newUser.email,
+            isAdmin: newUser.isAdmin,
+            token: token
+        });
     }
-}
+};
 
-export const userLogin = async (req,res)=>{
-    //desestructurar objeto request
-    const{email,password} = req.body
-    const user=await User.findOne({email})
+export const userLogin = async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
 
-    //si el usuario existe comparar ls hash(request,mongo)
-    if(user){
-        if(await bcrypt.compare(password , user.password)){
+    if (user) {
+        if (await bcrypt.compare(password, user.password)) {
             res.status(200).json({
                 id: user._id,
                 name: user.firstName,
-                token: generarToken( user._id)
-            })
-        }else{
+                token: generarToken(user._id)
+            });
+        } else {
             res.status(404).json({
-                "message":"Credenciales invalidas"
-            })
+                message: "Credenciales inválidas"
+            });
         }
+    } else {
+        res.status(404).json({
+            message: "Usuario no encontrado"
+        });
     }
-}
+};
 
-//funcion que retorne el token 
-const generarToken = (id) =>{
-    return jwt.sign({id},process.env.JWT_SECRET,{
-        expiresIn:'30d'
-    })
-}
+const generarToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '30d'
+    });
+};
